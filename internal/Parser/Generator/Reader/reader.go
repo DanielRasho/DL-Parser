e94eae2ebf0768455e4e19d6c475eb9ec20f49aa
DanielRasho <ray22933@uvg.edu.gt>
2025-04-23 08:05:11 -0600

    feat: defition of yapar reader

---
+++ internal/Parser/Generator/Reader/reader.go
@@ -0,0 +1,6 @@
+package reader
+
+// TODO: Lo que tienes que implementar andre
+func Parse(filePath string) (*ParserDefinition, error) {
+	return nil, nil
+}
---
+++ internal/Parser/Generator/Reader/types.go
@@ -0,0 +1,34 @@
+package reader
+
+// Its a programatically representation of a yapar file.
+type ParserDefinition struct {
+	Tokens      []ParserSymbol
+	Productions []ParserProduction
+}
+
+// Represents a single production declaration
+//
+//	{Head : "A", Body: ["A",+"A"]}
+type ParserProduction struct {
+	Head ParserSymbol
+	// List of symbols that comprehend a production
+	Body []ParserSymbol
+}
+
+// Smallest information unit, the parser can read. Which is basically a symbol
+// which can a terminal or non terminal.
+// Example TERMINAL:
+//
+//	{Id: 2, Value: "NUMBER"}
+//
+// Example TERMINAL:
+//
+//	{Id: -1, Value: "A"}
+type ParserSymbol struct {
+	// If token is terminal, the id comes from the order of declaration in
+	// the Yapar file.
+	// Set to -1 if symbol is NON-TERMINAL
+	Id int
+	// The string value itself of the symbol
+	Value string
+}
