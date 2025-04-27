6484365a207cb7e9b6a5bf17c317dc7fe88dba63
DanielRasho <ray22933@uvg.edu.gt>
2025-04-26 21:31:44 -0600

    feat: add entrypoint FirstFollow

--- cmd/FirstFollow/main.go
+++ cmd/FirstFollow/main.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	generator "github.com/DanielRasho/Parser/internal/Parser/Generator"
+)
 
 func main() {
+	// Define the flags
+	fileFlag := flag.String("f", "", "Yalex file path")
+	outputFlag := flag.String("o", "", "Output file path")
 
-	fmt.Println("Hello world")
+	// Parse the command line flags
+	flag.Parse()
+
+	// Check if both flags are provided, if not print usage
+	if *fileFlag == "" || *outputFlag == "" {
+		fmt.Println("Usage: myprogram -f <input-file> -o <output-file>")
+		os.Exit(1)
+	}
+
+	// Print the values of the flags (just as an example)
+	fmt.Printf("Input file: %s\n", *fileFlag)
+	fmt.Printf("Output file: %s\n", *outputFlag)
+
+	// CODE FOR GENERATING LEXER ...
+	err := generator.Compile(*fileFlag, *outputFlag, true)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Print("hello?")
 }
